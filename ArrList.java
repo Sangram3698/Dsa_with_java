bce73335b9d99a62bf4cb9e50667bb10fffb1b15
Bhumi IT <sngrmbhandari@gmail.com>
2025-12-12 12:31:10 +0545

    array samma

---
+++ ArrList.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ArrList {
+    public static void main(String[] args) {
+        ArrayList<Integer> li=new ArrayList<>();
+        li.add(0);
+        li.add(1);
+        li.add(2);
+        li.add(3);
+        li.add(4);
+        li.add(5);
+
+        for(int values:li){
+            System.out.print(values+" ");
+        }
+
+    }
+}
